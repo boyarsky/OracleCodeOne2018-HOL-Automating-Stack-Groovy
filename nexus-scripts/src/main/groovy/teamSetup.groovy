15b2029f463ea44c03f1c925dfb56616d352a1e1
Jeanne Boyarsky <nyjeanne@gmail.com>
2018-09-11 20:27:00 -0400

    fix nexus script so access is correct for sea lions/ospreys

--- nexus-scripts/src/main/groovy/teamSetup.groovy
+++ nexus-scripts/src/main/groovy/teamSetup.groovy
@@ -5,63 +5,81 @@ import org.sonatype.nexus.common.app.*;
 import org.sonatype.nexus.common.entity.*
 import org.sonatype.nexus.security.authz.*
 
-// most of this code is based off of https://gist.github.com/nblair/1a0e05713c3edb7e5360c2b0222c7623
-// with changes for CSEL content selector syntax
-
+// much of this code is based off of https://gist.github.com/nblair/1a0e05713c3edb7e5360c2b0222c7623
 
 // use container.lookup to fetch internal APIs we need to use
 def selectorManager = container.lookup(SelectorManager.class.name)
 def securitySystem = container.lookup(SecuritySystem.class.name)
 def authorizationManager = securitySystem.getAuthorizationManager('default')
 
-// create content selector (if not already present)
-def selectorConfig = new SelectorConfiguration(
-    name: 'sea-lion-selector',
-    type: 'csel',
-    description: 'sea-lion packages',
-    attributes: ['expression': 'format == "maven2" and coordinate.groupId =^ "net.selikoff.oraclecodeone.groovy.sea-lion"']
-)
-if (selectorManager.browse().find { it -> it.name == selectorConfig.name } == null) {
-  selectorManager.create(selectorConfig)
-}
-
-// create content selector privilege for release repo
-def releaseProperties = ['contentSelector' : selectorConfig.name, 
-   'repository' : 'custom-releases', 'actions' : 'read,edit']
-def releasePrivilege = new org.sonatype.nexus.security.privilege.Privilege(
-	id: "sea-lion-release-priv",
-	version: '',
-	name: "sea-lion-release-priv",
-	description: "Content Selector Release privilege",
-	type: "repository-content-selector",
-	properties: releaseProperties
-)
-authorizationManager.addPrivilege(releasePrivilege)
-
-// create content selector privilege for snapshot repo
-def snapshotProperties = ['contentSelector' : 'sea-lion-selector',
-	'repository' : 'custom-snapshots', 'actions' : 'read,edit']
-def snapshotPrivilege = new org.sonatype.nexus.security.privilege.Privilege(
-	id: "sea-lion-snapshot-priv",
-	version: '',
-	name: "sea-lion-snapshot-priv",
-	description: "Content Selector Snapshot privilege",
-	type: "repository-content-selector",
-	properties: snapshotProperties
-)
-authorizationManager.addPrivilege(snapshotPrivilege)
-
-// create a role with the snapshot and release privileges
-def role = new org.sonatype.nexus.security.role.Role(
-	roleId: "sea-lion-role",
+// -------------------------------------------------------------------------
+// anonymous user cannot see osprey or sea lion code
+def limitedAnonRole = new org.sonatype.nexus.security.role.Role(
+	roleId: "limited-anon",
 	source: "Nexus",
-	name: "sea-lion-role",
-	description: "Sea Lion Role",
+	name: "Limited Anonymous",
+	description: "Browse access without access to all group ids",
 	readOnly: false,
-	privileges: [ snapshotPrivilege.id, releasePrivilege.id ],
+	privileges: [ 'nx-search-read', 'nx-repository-view-maven2-*-read' ],
 	roles: []
 )
-authorizationManager.addRole(role)
+authorizationManager.addRole(limitedAnonRole)
+
+// -------------------------------------------------------------------------
+
+// create content selector (if not already present)
+def ospreySelectorConfig = new SelectorConfiguration(
+	name: 'osprey-selector',
+	type: 'csel',
+	description: 'sea-lion packages',
+	attributes: ['expression': 'format == "maven2" and coordinate.groupId =^ "net.selikoff.oraclecodeone.groovy.osprey"']
+)
+if (selectorManager.browse().find { it -> it.name == ospreySelectorConfig.name } == null) {
+  selectorManager.create(ospreySelectorConfig)
+}
+
+// -------------------------------------------------------------------------
+
+// create privileges for release and snapshot repositories
+def ospreyReleaseProperties = ['contentSelector' : ospreySelectorConfig.name, 
+   'repository' : 'custom-releases', 'actions' : 'browse,read,edit']
+def ospreyReleasePrivilege = new org.sonatype.nexus.security.privilege.Privilege(
+	id: "osprey-release-priv",
+	version: '',
+	name: "osprey-release-priv",
+	description: "Content Selector Release privilege",
+	type: "repository-content-selector",
+	properties: ospreyReleaseProperties
+)
+
+def ospreySnapshotProperties = ['contentSelector' : 'osprey-selector',
+	'repository' : 'custom-snapshots', 'actions' : 'browse,read,edit']
+def ospreySnapshotPrivilege = new org.sonatype.nexus.security.privilege.Privilege(
+	id: "osprey-snapshot-priv",
+	version: '',
+	name: "osprey-snapshot-priv",
+	description: "Content Selector Snapshot privilege",
+	type: "repository-content-selector",
+	properties: ospreySnapshotProperties
+)
+authorizationManager.addPrivilege(ospreyReleasePrivilege)
+authorizationManager.addPrivilege(ospreySnapshotPrivilege)
+
+// -------------------------------------------------------------------------
+// create a role with the snapshot and release privileges
+def ospreyRole = new org.sonatype.nexus.security.role.Role(
+	roleId: "osprey-role",
+	source: "Nexus",
+	name: "osprey-role",
+	description: "Osprey Role",
+	readOnly: false,
+	privileges: [ ospreySnapshotPrivilege.id, ospreyReleasePrivilege.id ],
+	roles: []
+)
+authorizationManager.addRole(ospreyRole)
+
+// -------------------------------------------------------------------------
 
 // add a local user account with the role
-security.addUser("sam", "Sam", "S", "sam@none.com", true, "sam", [ 'sea-lion-role', 'nx-anonymous' ])
+security.addUser("olivia", "Olivia", "O", "olivia@none.com", true, "olivia", [ 'osprey-role', 'limited-anon' ])
+security.addUser("owen", "Owen", "O", "owen@none.com", true, "owen", [ 'osprey-role', 'limited-anon' ])
