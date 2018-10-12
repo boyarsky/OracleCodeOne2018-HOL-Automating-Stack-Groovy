610f43feaef782d8b43e2f9a752bd055cafa3d3a
Jeanne Boyarsky <nyjeanne@gmail.com>
2018-10-12 08:26:11 -0400

    allow script to be run more than once

--- nexus-scripts/src/main/groovy/nexusBasicSetup.groovy
+++ nexus-scripts/src/main/groovy/nexusBasicSetup.groovy
@@ -3,21 +3,33 @@ import org.sonatype.nexus.security.*
 import org.sonatype.nexus.repository.maven.*
 import org.sonatype.nexus.repository.storage.*
 import org.sonatype.nexus.blobstore.api.*
+import org.sonatype.nexus.repository.manager.*
+import org.sonatype.nexus.security.user.*
 
 // uncomment when need autocomplete (comment for commit so Nexus uses built in variables)
 // RepositoryApi repository;
 // SecurityApi security;
 
+repositoryManager = container.lookup(RepositoryManager.class.name)
+securitySystem = container.lookup(SecuritySystem.class.name)
+authorizationManager = securitySystem.getAuthorizationManager('default')
+
 // create repositories (using default blob-store since this is just a lab)
-repository.createMavenHosted("custom-snapshots", BlobStoreManager.DEFAULT_BLOBSTORE_NAME, true, VersionPolicy.SNAPSHOT, WritePolicy.ALLOW_ONCE, LayoutPolicy.STRICT)
+if (! repositoryManager.exists("custom-snapshots")) {
+  repository.createMavenHosted("custom-snapshots", BlobStoreManager.DEFAULT_BLOBSTORE_NAME, true, VersionPolicy.SNAPSHOT, WritePolicy.ALLOW_ONCE, LayoutPolicy.STRICT)
+}
 // release uses all the defaults so we don't have to specify all the parameters
-repository.createMavenHosted("custom-releases")
+if (! repositoryManager.exists("custom-releases")) {
+	repository.createMavenHosted("custom-releases")
+}
  
 
 // create a role with all Maven view/upload privileges
 createRole()
 
-security.addUser("jenkins", "Jenkins", "Integration", "none@email.com", true, "jenkins123", ["jenkins-role"])
+if (! securitySystem.searchUsers(new UserSearchCriteria('jenkins'))) {
+  security.addUser("jenkins", "Jenkins", "Integration", "none@email.com", true, "jenkins123", ["jenkins-role"])
+}
 
 def createRole() {
   privileges = [
@@ -27,6 +39,9 @@ def createRole() {
     "nx-repository-view-maven2-*-add",
     "nx-repository-view-maven2-*-edit"]
   nestedRoles = []
-  security.addRole("jenkins-role", "jenkins-role", "deploy to Maven repos", privileges, nestedRoles)
+  
+  if (! authorizationManager.listRoles().grep { it.roleId == 'jenkins-role' }) {
+    security.addRole("jenkins-role", "jenkins-role", "deploy to Maven repos", privileges, nestedRoles)
+  }	
 
 }
