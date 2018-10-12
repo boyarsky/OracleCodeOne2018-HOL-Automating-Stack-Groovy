42693ab6e8e19cc8e151110d3942ac33e398d8f5
Jeanne Boyarsky <nyjeanne@gmail.com>
2018-10-12 08:40:48 -0400

    restrict anon to limited access

--- nexus-scripts/src/main/groovy/nexusBasicSetup.groovy
+++ nexus-scripts/src/main/groovy/nexusBasicSetup.groovy
@@ -5,6 +5,7 @@ import org.sonatype.nexus.repository.storage.*
 import org.sonatype.nexus.blobstore.api.*
 import org.sonatype.nexus.repository.manager.*
 import org.sonatype.nexus.security.user.*
+import org.sonatype.nexus.security.role.*
 
 // uncomment when need autocomplete (comment for commit so Nexus uses built in variables)
 // RepositoryApi repository;
@@ -14,6 +15,8 @@ repositoryManager = container.lookup(RepositoryManager.class.name)
 securitySystem = container.lookup(SecuritySystem.class.name)
 authorizationManager = securitySystem.getAuthorizationManager('default')
 
+// -------------------------------------------------------------------------
+
 // create repositories (using default blob-store since this is just a lab)
 if (! repositoryManager.exists("custom-snapshots")) {
   repository.createMavenHosted("custom-snapshots", BlobStoreManager.DEFAULT_BLOBSTORE_NAME, true, VersionPolicy.SNAPSHOT, WritePolicy.ALLOW_ONCE, LayoutPolicy.STRICT)
@@ -22,16 +25,22 @@ if (! repositoryManager.exists("custom-snapshots")) {
 if (! repositoryManager.exists("custom-releases")) {
 	repository.createMavenHosted("custom-releases")
 }
+
+// -------------------------------------------------------------------------
  
+// limit access for anonymous
+updateAnonymousPrivileges()
 
 // create a role with all Maven view/upload privileges
-createRole()
+createJenkinsRole()
 
 if (! securitySystem.searchUsers(new UserSearchCriteria('jenkins'))) {
   security.addUser("jenkins", "Jenkins", "Integration", "none@email.com", true, "jenkins123", ["jenkins-role"])
 }
 
-def createRole() {
+// -------------------------------------------------------------------------
+
+def createJenkinsRole() {
   privileges = [
     "nx-search-read",
     "nx-repository-view-maven2-*-read",
@@ -44,4 +53,11 @@ def createRole() {
     security.addRole("jenkins-role", "jenkins-role", "deploy to Maven repos", privileges, nestedRoles)
   }	
 
+}
+// -------------------------------------------------------------------------
+def updateAnonymousPrivileges() {
+	def limitedAnonRole = new RoleIdentifier('default', 'limited-anon')
+	def anonUser = securitySystem.getUser('anonymous')
+	anonUser.setRoles([ limitedAnonRole ].toSet())
+	securitySystem.updateUser(anonUser)
 }
